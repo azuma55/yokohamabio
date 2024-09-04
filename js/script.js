ef35427b21b11670c0dce5273c092af10f43d2c1
azuma55 <60699928+azuma55@users.noreply.github.com>
2024-09-04 18:21:49 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+    // jQueryプログラムを記述。
+		$('.bxslider').bxSlider({
+			auto: true,
+			slideWidth: 960,
+			// minSlides: 2,
+			slideMargin:0,
+			pager:false,
+			controls:false,
+		});
+});
+
